b22bbb6221b468e5416b855d284c6bb3f04a0662
Dmitriy Vorotilin <dvor85@gmail.com>
2015-03-17 19:08:18 +0400

    resolved dependencies

--- default.py
+++ default.py
@@ -1,6 +1,25 @@
 ﻿# Copyright (c) 2013 Torrent-TV.RU
 # Writer (c) 2011, Welicobratov K.A., E-mail: 07pov23@gmail.com
 
+import os, sys
+
+REMOTE_DBG = True 
+
+# append pydev remote debugger
+if REMOTE_DBG:
+    # Make pydev debugger works for auto reload.
+    # Note pydevd module need to be copied in XBMC\system\python\Lib\pysrc
+    try:
+        import pysrc.pydevd as pydevd  # with the addon script.module.pydevd, only use `import pydevd`
+        # stdoutToServer and stderrToServer redirect stdout and stderr to eclipse console
+        pydevd.settrace('localhost', stdoutToServer=True, stderrToServer=True)
+    except:
+        t, v, tb = sys.exc_info()        
+        sys.stderr.write("Error: {0}:{1} | You must add org.python.pydev.debug.pysrc to your PYTHONPATH.".format(t, v))
+        import traceback
+        traceback.print_tb(tb)
+        sys.exit(1)
+
 import xbmc
 import xbmcaddon
 import cPickle
@@ -13,9 +32,10 @@ from okdialog import OkDialog
 def checkPort(params):
     if not defines.checkPort(params):
         
-        dialog = OkDialog("okdialog.xml", defines.SKIN_PATH, defines.ADDON.getSetting('skin'))
-        dialog.setText("Порт %s закрыт. Для стабильной работы сервиса и трансляций, настоятельно рекомендуется его открыть." % defines.ADDON.getSetting('outport'))
-        dialog.doModal()
+        #dialog = OkDialog("okdialog.xml", defines.SKIN_PATH, defines.ADDON.getSetting('skin'))
+        #dialog.setText("Порт %s закрыт. Для стабильной работы сервиса и трансляций, настоятельно рекомендуется его открыть." % defines.ADDON.getSetting('outport'))
+        #dialog.doModal()
+        pass
 
 if __name__ == '__main__':
     if not defines.ADDON.getSetting('skin'):
